0aac45f38ca8fad4bd4584982d457b861e4fc02e
Tabitha Lee <2295373l@student.gla.ac.uk>
2017-02-06 21:58:56 +0000

    add page added

--- rango/wrango/views.py
+++ rango/wrango/views.py
@@ -66,9 +66,9 @@ def add_page(request, category_name_slug):
                 page.category = category
                 page.views = 0
                 page.save()
-                return show_category(request, category_name_slug)
-            else:
+            return show_category(request, category_name_slug)
+        else:
                 print(form.errors)
 
-        context_dict = {'form': form, 'category': category}
-        return render(request, 'rango/add_page.html', context_dict)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
